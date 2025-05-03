ad75af84f0df9d720d64f90a75838268ecb285e3
Andoni Arrabal <andoni.arrabal@opendeusto.es>
2025-05-03 07:45:36 +0200

    funcion preferencias

--- skyscanner/src/skyscanner/Main.java
+++ skyscanner/src/skyscanner/Main.java
@@ -4,15 +4,18 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.io.FileInputStream;
 
 
 public class Main {
 	
-	public static HashMap<String, Float> ordenarDestinos(Trip viaje, HashMap<PreferenciaEnum, Float>mapa){
+	public static LinkedHashMap<String, Float> ordenarDestinos(Trip viaje, HashMap<PreferenciaEnum, Float>mapa){
 		HashMap<String, Float> mapaDestinos = new HashMap<>();
-		
+		PreferenciaEnum[] preferencias = PreferenciaEnum.values();
 		for (String destino : viaje.getDestination()) {
 			try {
 				Scanner sc = new Scanner(new FileInputStream("preferencias.csv"));
@@ -22,28 +25,24 @@ public class Main {
 					String[] campos = linea.split(",");
 					
 					String destinoViaje = campos[0];
-					
+					float puntuacion = 0;
 					if(destino.equals(destinoViaje)) {
-						int seguridad = Integer.parseInt(campos[1]);
-						int costo = Integer.parseInt(campos[2]);
-						int accesibilidad = Integer.parseInt(campos[3]);
-						int clima = Integer.parseInt(campos[4]);
-						int comida = Integer.parseInt(campos[5]);
-						int cultura_arte = Integer.parseInt(campos[6]);
-						int actividades = Integer.parseInt(campos[7]);
-						int playa = Integer.parseInt(campos[8]);
-						int montana_naturaleza = Integer.parseInt(campos[9]);
-						int vida_nocturna = Integer.parseInt(campos[10]);
-						
+						for (int i = 0; i < preferencias.length; i++) {
+						    PreferenciaEnum clave = preferencias[i];
+						    Float valor = mapa.get(clave);
+						    int valor2 = Integer.parseInt(campos[i+1]);
+						    puntuacion+=valor*valor2;
+						}
+						mapaDestinos.put(destinoViaje, puntuacion);
 					}
-					
 				}		
 			} catch (FileNotFoundException e) {
 				System.out.println("Error: no se ha podido abrir el fichero " + "preferencias.csv");
 			}
 		}
 		
-		return mapaDestinos;
+		return mapaDestinos.entrySet().stream().sorted(Map.Entry.<String, Float>comparingByValue().reversed()).collect
+				(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue,(e1, e2) -> e1,LinkedHashMap::new));
 	}
 	
 	public static HashMap<PreferenciaEnum, Float> preferenciaEquipo(Trip viaje){
